9028effcec944afb700d1f5f7f94b4b11cb4009b
EASWARAN-S <160413220+EASWARAN-S@users.noreply.github.com>
2024-04-17 19:00:16 +0530

    Add files via upload

---
+++ DAY_7_17_04_2024/leetCode/FinalValueAfterOperations.java
@@ -0,0 +1,17 @@
+public class FinalValueAfterOperations {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].equals("++X"))
+                ++x;
+            if (operations[i].equals("X++"))
+                x++;
+            if (operations[i].equals("--X"))
+                --x;
+            if (operations[i].equals("X--"))
+                x--;
+
+        }
+        return x;
+    }
+}
